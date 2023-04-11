596ad455e0d21b62d619d8c5564b78766f8a1c2b
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-04-11 18:55:13 +0000

    configuration attributes for wrapper

--- trainer/main/configs.py
+++ trainer/main/configs.py
@@ -1,5 +1,9 @@
+# TODO fix mypy that does not recognize correctly the types i.e. Stateless
+# type: ignore
 from trainer.config.main import ConfigBase, configclass
-from trainer.config.types import Derived, Optional, Stateless
+from trainer.config.types import Optional, Stateless, Literal
+from trainer.modules.optimizer import OptimizerConfig
+from trainer.modules.scheduler import SchedulerConfig
 
 
 @configclass
@@ -7,7 +11,9 @@ class TrainConfig(ConfigBase):
     dataset: str
     batch_size: int
     epochs: int
-    random_seed: Optional[int] = None
+    optimizer_config: OptimizerConfig
+    scheduler_config: Optional[SchedulerConfig]
+    rand_weights_init: bool = True
 
 
 @configclass
@@ -18,11 +24,24 @@ class ModelConfig(ConfigBase):
 @configclass
 class RunConfig(ConfigBase):
     # location to store experiment artifacts
-    experiment_dir: Stateless[str]
+    experiment_dir: Stateless[Optional[str]] = None
+    random_seed: Optional[int] = None
     train_config: TrainConfig
     model_config: ModelConfig
     keep_n_checkpoints: Stateless[int] = 3
-    tensorboard: bool = True
+    tensorboard: Stateless[bool] = True
+    amp: Stateless[bool] = True
+    device: Stateless[str] = "cuda"
+    verbose: Stateless[Literal["console", "tqdm", "silent"]] = "console"
+    eval_subsample: Stateless[float] = 1
+    metrics_n_batches: Stateless[int] = 32
+    metrics_mb_limit: Stateless[int] = 100
+    early_stopping_iter: Stateless[Optional[int]] = None
+    eval_epoch: Stateless[float] = 1
+    log_epoch: Stateless[float] = 1
+    init_chkpt: Stateless[Optional[str]] = None
+    warm_up_epochs: Stateless[float] = 1
+    divergence_factor: Stateless[float] = 100
 
     @property
     def uid(self) -> str:
