552d2ee6bee5ee7d9435abdab171661c718f60af
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-05-18 19:48:20 +0000

    resnet.py example

---
+++ examples/run.py
@@ -0,0 +1,62 @@
+import argparse
+from pathlib import Path
+import shutil
+
+
+from resnet import MyModel, MyModelWrapper, ResConfig, ResRunConfig
+
+from ablator import ParallelConfig, ParallelTrainer, ProtoTrainer, configclass
+
+WORKING_DIRECTORY = Path(__file__).parent
+
+
+@configclass
+class MyParallelConfig(ParallelConfig):
+    model_config: ResConfig
+
+
+def mp_train(mp_config):
+    wrapper = MyModelWrapper(
+        model_class=MyModel,
+    )
+    run_config = MyParallelConfig.load(mp_config)
+    shutil.rmtree(run_config.experiment_dir)
+    ablator = ParallelTrainer(
+        wrapper=wrapper,
+        run_config=run_config,
+    )
+
+    # NOTE to run on a cluster you will need to start ray with `ray start --head` and pass ray_head_address="auto"
+    ablator.launch(
+        working_directory=WORKING_DIRECTORY,
+        ray_head_address=None,
+    )
+    ablator.evaluate()
+
+
+def base_train(config):
+    wrapper = MyModelWrapper(
+        model_class=MyModel,
+    )
+    run_config = ResRunConfig.load(config)
+    shutil.rmtree(run_config.experiment_dir)
+    ablator = ProtoTrainer(
+        wrapper=wrapper,
+        run_config=run_config,
+    )
+    ablator.launch()
+
+
+def run(config: str, mp: bool):
+    if mp:
+        mp_train(config)
+    else:
+        base_train(config)
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", type=str, required=True)
+    args.add_argument("--mp", action="store_true")
+    kwargs = vars(args.parse_args())
+    config = run(**kwargs)
