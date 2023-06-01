51a2eb787db7f1c7ee66bcf94472b3b022b6f13a
liyuangang <1095350436@qq.com>
2023-05-31 18:04:10 -0700

    fix: fix problem about work directory

--- localtest/localtest.sh
+++ localtest/localtest.sh
@@ -5,7 +5,6 @@ IMAGE_NAME="ablator-local-test"
 CONTAINER_NAME="ablator-local-test"
 
 # Build the Docker image
-docker build -t $IMAGE_NAME .
-
+docker build -f Dockerfile -t $IMAGE_NAME ..
 # Run the Docker container
 docker run -it --rm --name $CONTAINER_NAME $IMAGE_NAME
