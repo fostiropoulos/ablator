208fe3e872cddeb699ff2af40de735f7a9d4be5f
liyuangang <1095350436@qq.com>
2023-06-01 14:22:52 -0700

    patch: add readme.md and after test del images

---
+++ localtest/README.md
@@ -0,0 +1,6 @@
+## Run pytest in local machine using docker
+### TL;DR:
+```bash
+chmod +x ./localtest.sh
+./localtest.sh
+```
--- localtest/localtest.sh
+++ localtest/localtest.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Define image and container names
-IMAGE_NAME="ablator-local-test"
-CONTAINER_NAME="ablator-local-test"
+length=5
+random_string=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w "$length" | head -n 1)
+
+IMAGE_NAME="ablator-local-test-${random_string}"
+CONTAINER_NAME="ablator-local-test-${random_string}"
 
 # Build the Docker image
 docker build -f Dockerfile -t $IMAGE_NAME ..
 # Run the Docker container
 docker run -it --rm --name $CONTAINER_NAME $IMAGE_NAME
+# Delete the Docker image after the container exits
+docker rmi "$IMAGE_NAME"
