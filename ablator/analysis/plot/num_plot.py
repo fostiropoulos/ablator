ed62d0dbf8278e8e45a584588bb21f044532711d
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-06-14 16:19:35 +0000

    use regplot from seaborn as an alternative

--- ablator/analysis/plot/num_plot.py
+++ ablator/analysis/plot/num_plot.py
@@ -15,6 +15,9 @@ class Numerical(Plot):
 
 
 class LinearPlot(Numerical):
+
+
+
     def _make(
         self,
         scatter_plot: bool = True,
@@ -34,9 +37,9 @@ class LinearPlot(Numerical):
             ],
             axis=1,
         )
-        g = sns.lmplot(df, x="x", y="y", markers=".", scatter_kws={"alpha": 0.3})
-        self.ax = g.ax
-        self.figure = g.ax.figure
+        g = sns.regplot(df, x="x", y="y", ax = self.ax, marker=".", scatter_kws={"alpha": 0.3})
+        self.ax = g
+        self.figure = g.figure
 
         return self.figure, self.ax
 
