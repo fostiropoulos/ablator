b9b509a3b14b1d5fcfd0d9e4f328623424faf6a9
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-07-14 23:33:13 +0000

    update imports based on refactoring and provide a global `package_dir`

--- ablator/__init__.py
+++ ablator/__init__.py
@@ -1,15 +1,29 @@
+from pathlib import Path
+
 from ablator.analysis.plot.cat_plot import ViolinPlot
 from ablator.analysis.plot.main import PlotAnalysis
-from ablator.analysis.results import Results
 from ablator.analysis.plot.num_plot import LinearPlot
+from ablator.analysis.results import Results
 from ablator.config.main import ConfigBase, configclass
-from ablator.config.types import (Annotation, Derived, Dict, Enum, List,
-                                  Literal, Optional, Stateful, Stateless,
-                                  Tuple, Type)
-from ablator.main.configs import (ModelConfig, Optim, ParallelConfig,
-                                  RunConfig, TrainConfig)
+from ablator.config.mp import Optim
+from ablator.config.proto import ModelConfig, RunConfig, TrainConfig
+from ablator.config.types import (
+    Annotation,
+    Derived,
+    Dict,
+    Enum,
+    List,
+    Literal,
+    Optional,
+    Stateful,
+    Stateless,
+    Tuple,
+    Type,
+)
 from ablator.main.model.wrapper import ModelWrapper
-from ablator.main.mp import ParallelTrainer
+from ablator.main.mp import ParallelConfig, ParallelTrainer
 from ablator.main.proto import ProtoTrainer
 from ablator.modules.optimizer import OPTIMIZER_CONFIG_MAP, OptimizerConfig
 from ablator.modules.scheduler import SCHEDULER_CONFIG_MAP, SchedulerConfig
+
+package_dir = Path(__file__).parent
