ac54f272ca919bf84ae8406214ee8da165f6bb3a
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-07-15 00:07:05 +0000

    remote file logger

--- ablator/modules/loggers/file.py
+++ ablator/modules/loggers/file.py
@@ -1,7 +1,8 @@
-from datetime import datetime
 import os
-
+from datetime import datetime
 from pathlib import Path
+
+import ray
 
 
 class FileLogger:
@@ -61,7 +62,7 @@ class FileLogger:
                 f.write(f"{msg}\n")
 
     def _print(self, msg: str, verbose=False):
-        """ Print a message to the console.
+        """Print a message to the console.
 
         Parameters
         ----------
@@ -74,7 +75,7 @@ class FileLogger:
         if self.verbose or verbose:
             print(msg)
 
-    def info(self, msg: str, verbose=False):
+    def info(self, msg: str, verbose=False) -> str:
         """Log an info message.
 
         Parameters
@@ -83,10 +84,15 @@ class FileLogger:
             The message to log.
         verbose : bool, optional
             Whether to print messages to the console, by default ``False``.
-        """
-        self(msg, verbose)
 
-    def warn(self, msg: str, verbose=True):
+        Returns
+        -------
+        str
+            the formated string message
+        """
+        return self(msg, verbose)
+
+    def warn(self, msg: str, verbose=True) -> str:
         """Log a warning message.
 
         Parameters
@@ -95,22 +101,32 @@ class FileLogger:
             The message to log.
         verbose : bool, optional
             Whether to print messages to the console, by default ``True``.
+
+        Returns
+        -------
+        str
+            the formated string message
         """
         msg = f"{FileLogger.WARNING}{msg}{FileLogger.ENDC}"
-        self(msg, verbose)
+        return self(msg, verbose)
 
-    def error(self, msg: str):
+    def error(self, msg: str) -> str:
         """Log an error message.
 
         Parameters
         ----------
         msg : str
             The message to log.
+
+        Returns
+        -------
+        str
+            the formated string message
         """
         msg = f"{FileLogger.FAIL}{msg}{FileLogger.ENDC}"
-        self(msg, True)
+        return self(msg, True)
 
-    def __call__(self, msg: str, verbose=True):
+    def __call__(self, msg: str, verbose=True) -> str:
         """Log a message.
 
         Parameters
@@ -119,14 +135,21 @@ class FileLogger:
             The message to log.
         verbose : bool, optional
             Whether to print messages to the console, by default True.
+
+
+        Returns
+        -------
+        str
+            the formated string message
         """
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         msg = f"{now}: {self.prefix}{msg}"
         self._write(msg)
         self._print(msg, verbose)
+        return msg
 
     def set_prefix(self, prefix: str | None = None):
-        """ Set the prefix for the logger.
+        """Set the prefix for the logger.
 
         Parameters
         ----------
@@ -139,7 +162,7 @@ class FileLogger:
             self.prefix = ""
 
     def set_path(self, path: str | Path):
-        """ Set the path to the log file.
+        """Set the path to the log file.
 
         Parameters
         ----------
@@ -154,3 +177,46 @@ class FileLogger:
 
         with open(path, mode, encoding="utf-8") as f:
             f.write(f"Starting Logger {now} \n")
+
+
+class RemoteFileLogger(FileLogger):
+    def __init__(
+        self,
+        path: str | Path | None = None,
+        verbose: bool = True,
+        prefix: str | None = None,
+    ):
+        super().__init__(path, verbose, prefix)
+        self._file_logger: ray.RemoteFunction[FileLogger]
+
+    def to_remote(self, address: str | None = None):
+        if address is None:
+            address, _ = ray.get_runtime_context().gcs_address.split(":")
+        self._file_logger = (
+            ray.remote(FileLogger)
+            .options(resources={f"node:{address}": 0.001})
+            .remote(
+                self.path,
+                self.verbose,
+                self.prefix[: -len(" - ")] if self.prefix is not None else None,
+            )
+        )
+
+    def __call__(self, msg: str, verbose=True):
+        if hasattr(self, "_file_logger"):
+            msg = ray.get(self._file_logger.__call__.remote(msg, False))
+            self._print(msg, verbose)
+        else:
+            super().__call__(msg, verbose=verbose)
+
+    def set_path(self, path: str | Path):
+        if hasattr(self, "_file_logger"):
+            ray.get(self._file_logger.set_path.remote(path))
+        else:
+            super().set_path(path)
+
+    def set_prefix(self, prefix: str | None = None):
+        if hasattr(self, "_file_logger"):
+            ray.get(self._file_logger.set_prefix.remote(prefix))
+        else:
+            super().set_prefix(prefix)
