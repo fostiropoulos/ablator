0aceb80aa9ac09dd476186e582d290208def42c8
h21565897 <h21565897@gmail.com>
2023-07-14 17:40:15 -0700

    add rclone tests, add github action support

--- ablator/modules/storage/rclone.py
+++ ablator/modules/storage/rclone.py
@@ -6,6 +6,7 @@ import sys
 import threading
 from ablator.modules.loggers.file import FileLogger
 import time
+import subprocess
 
 
 @configclass
@@ -21,17 +22,25 @@ class RcloneConfig(ConfigBase):
         return f"{self.config_name}:{self.remote_path}"
 
     def startMount(self, expriement_dir: Path, verbose=False):
+        self.rcloneWrapper.run_cmd("lsd", [self.get_remote_path_prefix()])
         self.experiment_dir = str(expriement_dir)
         if sys.platform == "win32":
-            self.rcloneProcess = self.rcloneWrapper.mount(f"{self.get_remote_path_prefix()}", self.experiment_dir, ["--rc",
-                                                                                                                    "--gcs-bucket-policy-only",
-                                                                                                                    "--vfs-cache-mode", "writes", "--dir-cache-time", "10s",
-                                                                                                                    "--poll-interval", "10s", "-o", 'FileSecurity=D:P(A;;FA;;;WD)',
-                                                                                                                    "--stats", "10s", "--transfers", "64", "-vv"], verbose=verbose)
+            command = ["--rc",
+                       "--gcs-bucket-policy-only",
+                       "--vfs-cache-mode", "writes", "--dir-cache-time", "10s",
+                       "--poll-interval", "10s", "-o", 'FileSecurity=D:P(A;;FA;;;WD)',
+                       "--stats", "10s", "--transfers", "64"]
+            if verbose is True:
+                command.append("-vv")
+            self.rcloneProcess = self.rcloneWrapper.mount(f"{self.get_remote_path_prefix()}", self.experiment_dir, command, verbose=verbose)
         else:
-            self.rcloneProcess = self.rcloneWrapper.mount(f"{self.get_remote_path_prefix()}", self.experiment_dir, [
-                "--rc", '--gcs-bucket-policy-only', '--dir-cache-time', '10s', '--poll-interval', '10s', '--vfs-cache-mode', 'writes', "--transfers", "64"], verbose=verbose)
-        for i in range(5, 0, -1):
+            subprocess.run(["umount", "-f", self.experiment_dir])
+            command = [
+                "--rc", '--gcs-bucket-policy-only', '--dir-cache-time', '10s', '--poll-interval', '10s', '--vfs-cache-mode', 'writes', "--transfers", "64"]
+            if verbose is True:
+                command.append("-vv")
+            self.rcloneProcess = self.rcloneWrapper.mount(f"{self.get_remote_path_prefix()}", self.experiment_dir, command, verbose=verbose)
+        for i in range(10, 0, -1):
             print("wait for rclone mouting", i)
             time.sleep(1)
         if self.inspect_mount_status() is not True:
--- tests/modules/test_rclone.py
+++ tests/modules/test_rclone.py
@@ -1,15 +1,45 @@
 from pathlib import Path
-from ablator.main.configs import GcsRcloneConfig
+from ablator.main.configs import RemoteRcloneConfig
 from ablator.modules.storage.rclone import RcloneConfig
+import sys
+import shutil
+import os
 
 
-def test_rclone_gcs(tmp_path: Path):
-    gcs_config = GcsRcloneConfig(
-        bucket="ablator_example_bucket_3",
-        service_account_file="./hjzcerti.json",
+def assert_error_msg(func: callable, msg: str):
+    try:
+        func()
+        assert False
+    except Exception as e:
+        if not msg == str(e):
+            raise e
+
+
+def test_rclone(tmp_path: Path):
+    if sys.platform != "linux":
+        return
+
+    remote_path = tmp_path / "mntremote"
+    local_path = tmp_path/"mntlocal"
+    shutil.rmtree(remote_path, ignore_errors=True)
+    shutil.rmtree(local_path, ignore_errors=True)
+    os.makedirs(remote_path, exist_ok=True)
+    os.makedirs(local_path, exist_ok=True)
+    gcs_config = RemoteRcloneConfig(
+        host="localhost",
+        key_file="~/.ssh/id_rsa",
+        user="",
+        remote_path=str(remote_path),
     )
-    gcs_config.startMount(tmp_path, verbose=False)
+    gcs_config.startMount(local_path, verbose=False)
+    assert gcs_config.inspect_mount_status() is True
+
+    shutil.rmtree(remote_path, ignore_errors=True)
+    shutil.rmtree(local_path, ignore_errors=True)
+    os.makedirs(local_path, exist_ok=True)
+    assert_error_msg(lambda: gcs_config.startMount(local_path, verbose=False),
+                     "rclone running with error, please check the config again!"),
 
 
 if __name__ == "__main__":
-    test_rclone_gcs(Path("/mounttest"))
+    test_rclone(Path.home())
