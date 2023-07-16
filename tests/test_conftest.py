838ce6c97f5d05af93767b77c0bb6511e0107f55
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-07-16 17:29:42 +0000

    testing random seed for conftest

---
+++ tests/test_conftest.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+import random
+
+
+def _assert_np_random():
+    np_rand = np.random.random()
+    assert np.isclose(np_rand, 0.417022004702574)
+
+
+def _assert_py_random():
+    rand = random.random()
+    assert np.isclose(rand, 0.13436424411240122)
+
+
+def _assert_torch_random():
+    rand_torch = torch.rand(1).item()
+    assert np.isclose(rand_torch, 0.7576315999031067)
+
+
+def test_conftest():
+    _assert_np_random()
+    _assert_py_random()
+    _assert_torch_random()
+
+def test_conftest_order():
+    _assert_torch_random()
+    _assert_py_random()
+    _assert_np_random()
+
+
+if __name__ == "__main__":
+    random.seed(1)
+    np.random.seed(1)
+    torch.manual_seed(1)
+    test_conftest()
