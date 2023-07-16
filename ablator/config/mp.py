91e1b50dafce8cc882cfe129ecb25356a8acd2bc
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-07-16 17:06:10 +0000

    documentation

--- ablator/config/mp.py
+++ ablator/config/mp.py
@@ -2,22 +2,31 @@ from ablator.config.hpo import SearchSpace
 from ablator.config.main import configclass
 from ablator.config.proto import RunConfig
 from ablator.config.types import Dict, Enum, Optional, Stateless
-from ablator.modules.storage.cloud import GcpConfig
 from ablator.modules.storage.remote import RemoteConfig
-
-CRASH_EXCEPTION_TYPES: list[type] = []
 
 
 class SearchAlgo(Enum):
     """
     Type of search algorithm.
-    TODO explain each type
+
+    Grid Sampling: Discretizes the search space into even intervals `n_bins`.
+    TPE Sampling: Tree-Structured Parzen Estimator [1] is a hyper-parameter optimization algorithm.
+    Random Sampling: Naively samples from the search space with a random probability.
+
+    The behavior of each algorithm depends highly on the budget allocated for each trial. For example,
+    Grid Sampling will repeat sampled configurations only after it has exhaustively evaluated the current
+    configuration space.
+
+    TPE and Random Sampling can repeat configurations at random.
+
+    References:
+    [1] Bergstra, James S., et al. “Algorithms for hyper-parameter optimization.”
+    Advances in Neural Information Processing Systems. 2011.
     """
 
     random = "random"
     tpe = "tpe"
     grid = "grid"
-    discrete = "discrete"
 
 
 class Optim(Enum):
@@ -35,29 +44,27 @@ class Optim(Enum):
 @configclass
 class ParallelConfig(RunConfig):
     """
-    Parallel training configuration. ``{"val_loss": "min"}``
+    Parallel training configuration.
 
     Attributes
     ----------
-    total_trials: int
+    total_trials: Optional[int]
         total number of trials.
     concurrent_trials: int
         number of trials to run concurrently.
     search_space: Dict[SearchSpace]
         search space for hyperparameter search,
         eg. ``{"train_config.optimizer_config.arguments.lr": SearchSpace(value_range=[0, 10], value_type="int"),}``
-    optim_metrics: Dict[Optim]
+    optim_metrics: Optional[Dict[Optim]]
         metrics to optimize, eg. ``{"val_loss": "min"}``
+    gpu_mb_per_experiment: int
+        CUDA memory requirement per experimental trial in MB. e.g. a value of 100 is equivalent to 100MB
     search_algo: SearchAlgo = SearchAlgo.tpe
         type of search algorithm.
     ignore_invalid_params: bool = False
         whether to ignore invalid parameters when sampling or raise an error.
-    sample_duplicate_params: bool = False
-        whether to include duplicate parameters when sampling or ingore them.
     remote_config: Optional[RemoteConfig] = None
         remote storage configuration.
-    gcp_config: Optional[GcpConfig] = None
-        gcp configuration.
 
     """
 
@@ -66,9 +73,6 @@ class ParallelConfig(RunConfig):
     search_space: Dict[SearchSpace]
     optim_metrics: Stateless[Optional[Dict[Optim]]]
     gpu_mb_per_experiment: Stateless[int]
-    # cpus_per_experiment: Stateless[float]
     search_algo: Stateless[SearchAlgo] = SearchAlgo.tpe
     ignore_invalid_params: Stateless[bool] = False
-    sample_duplicate_params: Stateless[bool] = False
     remote_config: Stateless[Optional[RemoteConfig]] = None
-    gcp_config: Stateless[Optional[GcpConfig]] = None
