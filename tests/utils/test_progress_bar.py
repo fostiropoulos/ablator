e5bb86dc0aecf8058343907b1d34ca0ba990625d
VickyShel <jiaxinxi@usc.edu>
2023-10-06 18:32:47 -0700

    test cases for progress_bar.py

--- tests/utils/test_progress_bar.py
+++ tests/utils/test_progress_bar.py
@@ -184,10 +184,11 @@ def test_get_last_line(tmpdir):
     # create test cases
     lines=["This is the first line\n","This is the second line\n","This is the last line"]
     # write test cases into the temporary file
-    with open(test_file,"w") as test_file:
-        test_file.writelines(lines)
+    with open(test_file,"w") as file:
+        file.writelines(lines)
     # test if the function could return the last line of file correctly
-    assert get_last_line(test_file)=="This is the last line"
+    result=get_last_line(test_file)
+    assert result=="This is the last line"
 
 def test_get_last_line_with_edge_cases(tmpdir):
     # edge test case: filename is None
@@ -207,6 +208,10 @@ def test_get_last_line_with_edge_cases(tmpdir):
     empty_file=Path(tmpdir,"empty.txt")
     empty_file.touch()
     assert get_last_line(empty_file)==""
+
+@pytest.fixture(autouse=True)
+def set_custom_env_variable():
+    os.environ['TERM'] = 'linux'
 
 def test_display_class_init_function():
     # test __init__ function of Display class
@@ -248,18 +253,6 @@ def test_display_class_update_screen_dims_function():
     ncols_update=display.ncols
     assert nrows_update!=nrows and ncols_update!=ncols
 
-def test_display_class_print_texts_function():
-    # test case of display class's print_texts function
-    # since the texts are displayed in the terminal by using this function
-    #   need to use pseudo-terminal to test if the result is correct
-    def child_process_print_texts_function():
-        display = Display()
-        texts = ["hello", "world", "!"]
-        display.print_texts(texts)
-    keys={"hello":0,"world":1,"!":2}
-    tui(child_process_print_texts_function,keys)
-
-#
 def display_class_close_function():
     display = Display()
     assert display.is_terminal==True
@@ -279,8 +272,6 @@ def tui(test_func,assertion_func):
                 [f_d], [], [], 1)
         except (KeyboardInterrupt, ValueError):
             print("ValueError")
-        except Exception:
-            print("exception 1")
         else:
             print("read from screen")
             try:
@@ -290,12 +281,26 @@ def tui(test_func,assertion_func):
                 for line in screen.display:
                     print(line)
             except Exception:
-                print("exception 2")
+                raise Exception
         print("Terminal Start")
         for line in screen.display:
             print(line)
         print("Terminal End")
         assertion_func(screen)
+
+def test_display_class_print_texts_function():
+    # test case of display class's print_texts function
+    # since the texts are displayed in the terminal by using this function
+    #   need to use pseudo-terminal to test if the result is correct
+    display = Display()
+    keys={'hello':0,'world':1,'!':2}
+    def child_process_print_texts_function():
+        texts = ["hello", "world", "!"]
+        display.print_texts(texts)
+    def assertion_print_texts_function(screen):
+        for key,value in keys.items():
+            assert key in screen.display[value]
+    tui(child_process_print_texts_function,assertion_print_texts_function)
 
 def test_progress_bar_class_init_function(tmpdir):
     # test case: if the attributes of an instance have all been initiated
@@ -315,15 +320,17 @@ def test_progress_bar_class_init_function(tmpdir):
 def test_progress_bar_class_init_function_display(tmpdir):
     # test case: if the texts of progress_bar have been correctly display after initiating( current_iteration=0)
     def child_process_progress_bar_init_function():
-        log_file=Path(tmpdir,"test.log")
-        progress_bar=ProgressBar(10,2,log_file,1,None,"111111")
+        progress_bar_._update()
 
-    def assertion(screen):
+    def assertion_init_function_display(screen):
         for key, value in keys.items():
-            assert key in screen.display[value] or key==screen.display[value]
+            # breakpoint()
+            assert re.match(key,screen.display[value])
 
-    keys={"111111":1}
-    tui(child_process_progress_bar_init_function,assertion)
+    log_file=Path(tmpdir,"test.log")
+    progress_bar_=ProgressBar(10,2,log_file,1,None,"111111")
+    keys={r'111111:\s*0%\|\s*\| 0/2 \[00:00<\?, \?it/s, Remaining: \?\?\]\s*':1}
+    tui(child_process_progress_bar_init_function,assertion_init_function_display)
 
 def test_progress_bar_class_reset_function(tmpdir):
     # test case: if the current_iteration attribute of instance is resetting to 0 correctly
@@ -374,11 +381,15 @@ def progress_bar_class_make_metrics_message():
         "metric1": 123,
         "metric2": 456.789,
         "metric3": "value1",
-        "metric4": "value2"
+        "metric4": "value2",
+        "metric5": "value1",
+        "metric6": "value2"
     }
     expected_result = "metric1:  00000123 | metric2:  456.7890"
     result = ProgressBar.make_metrics_message(metrics, ncols=50, nrows=1)
+    print(result)
     assert re.match(result[0],expected_result) and len(result)==1
+
 
 def test_progress_bar_class_ncols():
     # test case: if ncols is returned correctly when display is not None
@@ -394,7 +405,7 @@ def test_progress_bar_class_nrows():
     # test case: if nrows is returned correctly when display is not None
     progress_bar=ProgressBar(10)
     assert progress_bar.display is not None
-    assert progress_bar.nrows==progress_bar.display.nrows
+    assert progress_bar.nrows==progress_bar.display.nrows-5
 
     # test case: if nrows is returned as None when display is None
     progress_bar.display=None
@@ -402,42 +413,34 @@ def test_progress_bar_class_nrows():
 
 def test_progress_bar_class_make_print_message(tmpdir):
     # test case of make_print_message when uid is not None
-    keys_uid={"":0,r'111111:\s*0%\|\s*\| 0/2 \[00:00<\?, \?it/s, Remaining: \?\?\]\s*':1}
-    def child_process_uid():
-        log_file = Path(tmpdir, "test.log")
-        progress_bar = ProgressBar(10, 2, log_file, 1, None, "111111")
-    def assertion_uid(screen):
-        for key,value in keys_uid.items():
-            if value==0:
-                assert screen.display[0].isspace()
-            elif value==1:
-                assert re.match(key,screen.display[value])
-
-    tui(child_process_uid,assertion_uid)
+    keys_uid=[r'111111:\s*0%\|\s*\| 0/2 \[00:00<\?, \?it/s, Remaining: \?\?\]\s*']
+    log_file = Path(tmpdir, "test.log")
+    progress_bar = ProgressBar(10, 2, log_file, 1, None, "111111")
+    texts=progress_bar.make_print_message()
+    print('texts texts texts texts')
+    print(texts)
+    assert texts[0]==''
+    assert re.match(keys_uid[0], texts[1])
 
     # test case of make_print_message when uid is None
-    keys={r'\s*0%\|\s*\| 0/2 \[00:00<\?, \?it/s, Remaining: \?\?\]\s*':1}
-    def child_process():
-        log_file = Path(tmpdir, "test.log")
-        progress_bar = ProgressBar(10, 2, log_file, 1, None)
-    def assertion(screen):
-        for key,value in keys.items():
-            if value==0:
-                assert screen.display[0].isspace()
-            elif value==1:
-                assert re.match(key,screen.display[value])
-    tui(child_process,assertion)
-
+    keys=[r'\s*0%\|\s*\| 0/2 \[00:00<\?, \?it/s, Remaining: \?\?\]\s*']
+    progress_bar = ProgressBar(10, 2, log_file, 1, None)
+    texts = progress_bar.make_print_message()
+    print('texts texts texts texts')
+    print(texts)
+    assert texts[0]==''
+    assert re.match(keys[0], texts[1])
 
 def test_progress_bar_class_update(tmpdir):
     # test case: if the texts have been displayed after running _update function
-    def child_process():
-        log_file = Path(tmpdir, "test.log")
-        progress_bar = ProgressBar(10, 2, log_file, 1, None, "111111")
-    def assertion(screen):
+    log_file = Path(tmpdir, "test.log")
+    progress_bar = ProgressBar(10, 2, log_file, 1, None, "111111")
+    def child_process_update():
+        progress_bar._update()
+    def assertion_update(screen):
         assert screen.display[1].isspace() is False
 
-    tui(child_process, assertion)
+    tui(child_process_update, assertion_update)
 
 def test_progress_bar_class_update_metrics(tmpdir):
     # test case: if the _update function is executed when current_iteration is 0
@@ -454,13 +457,11 @@ def test_progress_bar_class_update_metrics(tmpdir):
 def test_progress_bar_class_update_metrics_interval(tmpdir):
     # test case: if the _update function is executed when time.time() - self._prev_update_time > self.update_interval
     log_file = Path(tmpdir, "test.log")
-    keys_interval={r'metric:\s+value':0,r'111111:\s+100%\|██████████\| 2/2 \[\d+:\d+<\d+:\d+, \d+\.\d+it/s, Remaining: \d+:\d+\]\s*':1}
+    keys_interval={r'metric:\s+value':0, r'111111:\s+100%\|██████████\| 2/2 \[\d+:\d+<\d+:\d+, \d+\.\d+it/s, Remaining: \d+:\d+\]\s*':1}
     def child_process_update_metrics_2():
         progress_bar_interval.update_metrics({"metric":"value"},current_iteration=2)
     def assertion_update_metrics_2(screen):
         for key, value in keys_interval.items():
-            print("key")
-            print(key)
             assert re.match(key, screen.display[value])
     progress_bar_interval = ProgressBar(10, 2, log_file, 0, None, "111111")
     tui(child_process_update_metrics_2, assertion_update_metrics_2)
@@ -479,6 +480,35 @@ def test_progress_bar_class_update_metrics_current_iteration(tmpdir):
             assert re.match(key, screen.display[value])
     progress_bar = ProgressBar(10, 2, log_file, 1, None, "111111")
     tui(child_process_update_metrics_2, assertion_update_metrics_2)
+
+@pytest.fixture(scope="module")
+def ray_start():
+    ray.init(local_mode=True)
+
+# def test_remote_progress_bar_init_function(ray_start):
+#     remote_progress_bar=RemoteProgressBar.remote(10)
+#     bar_id=ray.put(remote_progress_bar)
+#     remote_progress_bar=ray.get(bar_id)
+#     print(type(remote_progress_bar))
+#     assert isinstance(ray.get(remote_progress_bar),RemoteProgressBar)
+#     assert isinstance(ray.get(remote_progress_bar.start_time.remote()),float)
+#     assert ray.get(remote_progress_bar.total_trials.remote())==10
+#     assert ray.get(remote_progress_bar.closed.remote())["not_existed_value"]==False
+#     assert ray.get(remote_progress_bar.texts.remote())["not_existed_value"]==[]
+#     assert ray.get(remote_progress_bar.finished_trials.remote())==0
+
+def test_remote_progress_bar_make_bar_function(ray_start):
+    remote_progress_bar=RemoteProgressBar.remote(10)
+    bar=ray.get(remote_progress_bar.make_bar.remote())
+    assert re.match(r'\s*0%\|\s*\| 0/10 \[00:00<\?, \?it/s, Remaining: \?\?\]\s*',bar)
+
+def remote_progress_bar_current_iteration_function(ray_start):
+    remote_progress_bar=RemoteProgressBar.remote(10)
+    iteration=ray.get(remote_progress_bar.current_iteration)
+    assert iteration==0
+
+
+
 
 if __name__ == "__main__":
     tmp_path = Path("/tmp/")
