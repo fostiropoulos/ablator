a7c7f7fa416459c267d8511b888cb3ae24af2d52
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-12-02 18:17:21 +0000

    fix packaging name conflicts and versioning

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Makes a copy of the same wheel file but renames it so that it is published only
 # for the supported OS, which is linux and macos.
-python -m build
-og_whl=$(ls dist/*-any.whl | sort | tail -n1)
-linux_whl=$(echo "$og_whl" | sed "s/any.whl/manylinux1_x86_64.whl/g")
-macos_10_whl=$(echo "$og_whl" | sed "s/any.whl/macosx_10_9_x86_64.whl/g")
-macos_11_whl=$(echo "$og_whl" | sed "s/any.whl/macosx_11_0_arm64.whl/g")
-cp $og_whl $linux_whl
-cp $og_whl $macos_10_whl
-cp $og_whl $macos_11_whl
-rm $og_whl
+
+dists=( "manylinux2014_x86_64" "manylinux2014_i686" "manylinux2014_aarch64" "manylinux2014_armv7l" "manylinux2014_ppc64" "manylinux2014_ppc64le" "manylinux2014_s390x" "macosx_10_9_x86_64" "macosx_11_0_arm64" )
+for dist in "${dists[@]}"
+do
+    python -m build
+    og_whl=$(ls dist/*-any.whl | sort | tail -n1)
+    dist_whl=$(echo "$og_whl" | sed "s/any.whl/$dist.whl/g")
+    mv $og_whl $dist_whl
+done
